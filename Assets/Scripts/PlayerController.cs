161ffb057e13436f4b0d1fde775742bbbf5eb605
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-09-03 14:35:34 +0800

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float pullForce;
+    
+    private Rigidbody2D rb2D;
+
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // Move the object
+        rb2D.velocity = -transform.up * moveSpeed;
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+﻿# Magnetized game
