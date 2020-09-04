4c73a14c56de5af6dc0cce3e4113a72e5c1a150f
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-09-04 15:54:51 +0800

    Add walls and goal

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,4 +18,21 @@ public class PlayerController : MonoBehaviour
         // Move the object
         rb2D.velocity = -transform.up * moveSpeed;
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            //Hide game object
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Goal"))
+        {
+            Debug.Log("Level clear");
+        }
+    }
 }
