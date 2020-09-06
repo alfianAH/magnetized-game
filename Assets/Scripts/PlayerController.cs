c1a505df392f7e6b82ae407ba7cb13781f0569fa
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-09-06 17:27:33 +0800

    Add magnet tower and control the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float pullForce;
-    
+    [SerializeField] private float moveSpeed = 5f,
+        pullForce = 100f,
+        rotateSpeed = 360f;
+
+    private GameObject closestTower,
+        hookedTower;
     private Rigidbody2D rb2D;
 
+    private bool isPulled = false;
+    
     private void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
@@ -17,6 +22,35 @@ public class PlayerController : MonoBehaviour
     {
         // Move the object
         rb2D.velocity = -transform.up * moveSpeed;
+
+        if (Input.GetKey(KeyCode.Z) && !isPulled)
+        {
+            if (closestTower && !hookedTower)
+            {
+                hookedTower = closestTower;
+            }
+
+            if (hookedTower)
+            {
+                float distance = Vector2.Distance(transform.position, hookedTower.transform.position);
+                
+                // Gravitation toward tower
+                Vector3 pullDirection = (hookedTower.transform.position - transform.position).normalized;
+                float newPullForce = Mathf.Clamp(pullForce / distance, 20, 50);
+                rb2D.AddForce(pullDirection * newPullForce);
+                
+                // Angular velocity
+                rb2D.angularVelocity = -rotateSpeed / distance;
+                
+                isPulled = true;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            isPulled = false;
+            hookedTower = null;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -33,6 +67,31 @@ public class PlayerController : MonoBehaviour
         if (other.CompareTag("Goal"))
         {
             Debug.Log("Level clear");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Tower"))
+        {
+            closestTower = other.gameObject;
+            
+            // Change tower's color back to green as indicator
+            other.gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(isPulled) return;
+
+        if (other.CompareTag("Tower"))
+        {
+            closestTower = null;
+            hookedTower = null;
+            
+            // Change tower's color back to normal
+            other.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
         }
     }
 }
