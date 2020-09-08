534628fa2bcb2d5de7c5ffc4e2df82672ab3876c
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-09-08 21:27:04 +0800

    Add Level in SelectLevelScene

---
+++ Assets/Scripts/SelectLevel/LevelScriptableObject.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Level", fileName = "NewLevel")]
+public class LevelScriptableObject : ScriptableObject
+{
+    public string levelName;
+    public string sceneName;
+}
---
+++ Assets/Scripts/SelectLevel/LevelUI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelUI : MonoBehaviour
+{
+    public LevelScriptableObject levelScriptableObject;
+
+    [SerializeField] private Text levelText;
+
+    private void Awake()
+    {
+        levelText.text = levelScriptableObject.levelName;
+    }
+}
