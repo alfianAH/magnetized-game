1584b7a496e1af9e071ebbd92f740588f29a712e
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-09-08 22:28:15 +0800

    Add Home Button in Pause prefab

--- Assets/Scripts/UIControllerScript.cs
+++ Assets/Scripts/UIControllerScript.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UIControllerScript : MonoBehaviour
 {
-    public GameObject pausePanel,
+    public GameObject homeBtn,
+        pausePanel,
         resumeBtn,
         levelClearText;
 
